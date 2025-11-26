b6a632b4404f8c90085575fbbb7a5e4532eff8b2
ranjay17 <sranjay15@gmail.com>
2025-11-27 00:23:11 +0530

    Signup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mailbox Client
+
+### Signup Page
+- Users can create a new account using **Email**, **Password**, and **Confirm Password**.
+- All fields are **mandatory**.
+- Client-side validation:
+  - Empty field check
+  - Password and Confirm Password match check
+- Firebase Authentication API used for creating new users.
+- On successful signup:
+  - Shows a success alert
+  - Logs `User has successfully signed up` in console.
---
+++ src/components/Signup.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+
+const Signup = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSignup = async(event) =>{
+        event.preventDefault()
+        if(!email || !password || !confirmPassword){
+            alert("All fields are mandatory!");
+            return
+        }
+        if(password != confirmPassword){
+            alert("Passwords do not match!");
+            return
+        }
+        try {
+            const response = await fetch(
+              "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDIWIv4gF6B5o-mi3BCyYap3ATb3fEsYeY",
+              {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body : JSON.stringify({
+                    email,
+                    password,
+                    returnSecureToken: true,
+                })
+              }
+            );
+            const data = await response.json();
+            if (!response.ok) {
+              alert(data.error?.message || "Signup failed");
+              console.log("Signup Failed:", data);
+              return; 
+            }
+            alert("Signup successfull")
+            console.log("User has successfully signed up:", data);
+        } catch (error) {
+            console.log(error)
+        }
+        setEmail("")
+        setPassword("")
+        setConfirmPassword("")
+    }
+  return (
+    <Container className="d-flex justify-content-center align-items-center mt-5">
+      <Card style={{ width: "400px" }} className="p-4 shadow">
+        <h3 className="text-center mb-3">SignUp</h3>
+
+        <Form onSubmit={handleSignup}>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="w-100 mt-2">
+            Sign up
+          </Button>
+        </Form>
+
+        <Button variant="success" className="w-100 mt-3">
+          Have an account? Login
+        </Button>
+      </Card>
+    </Container>
+  );
+}
+
+export default Signup
